0ef8b0629e75c216db39fc31bb881efbbd56b68b
David Miller <david@davidmiller.co>
2015-02-19 10:25:12 +1100

    changes to js dependencies

--- Fresh.Web/Fresh.Web/Scripts/_references.js
+++ Fresh.Web/Fresh.Web/Scripts/_references.js
@@ -1,7 +1,3 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="jquery-1.10.2.js" />
-/// <reference path="jquery-2.1.3.js" />
-/// <reference path="npm.js" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="modernizr-2.8.3.js" />
 /// <reference path="../gulpfile.js" />
