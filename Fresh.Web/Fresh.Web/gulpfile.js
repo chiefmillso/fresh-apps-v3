20a7f89538d57036aeaa83b96f7c84e57d04db17
David Miller <david@davidmiller.co>
2015-02-19 10:25:12 +1100

    changes to js dependencies

--- Fresh.Web/Fresh.Web/gulpfile.js
+++ Fresh.Web/Fresh.Web/gulpfile.js
@@ -1,21 +1,28 @@
-/// <vs AfterBuild='compile' />
+/// <vs AfterBuild='build' />
 var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var bowerFiles = require('gulp-bower-files');
 
 var paths = {
     scripts: ['client/js/**/*.js', '!client/external/**/*.coffee'],
     images: 'client/img/**/*'
 };
 
-gulp.task('clean', function () {
-
+gulp.task('lint', function () {
+    gulp.src(['./Scripts/**/*.js', '!./bower_components/**', '!./Scripts/lib/**'])
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('scripts', function () {
-
+// task
+gulp.task('bower-files', function () {
+    bowerFiles().pipe(gulp.dest("./Scripts/lib"));
 });
 
-gulp.task('compile', function () {
+gulp.task('build', ['bower-files','lint'])
 
-});
-
-gulp.task('default', ['compile', 'scripts']);
+gulp.task('default', ['build']);
