6e30e6c7ac3b66b1f61ab4c4f3bfcfa19bc21b97
David Miller <david@davidmiller.co>
2015-02-20 03:14:10 +1100

    added bootstrap to the website

--- Fresh.Web/Fresh.Web/gulpfile.js
+++ Fresh.Web/Fresh.Web/gulpfile.js
@@ -4,11 +4,28 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
-var bowerFiles = require('gulp-bower-files');
+var bower = require('gulp-bower-files');
+var gulpFilter = require('gulp-filter');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 
-var paths = {
-    scripts: ['client/js/**/*.js', '!client/external/**/*.coffee'],
-    images: 'client/img/**/*'
+var src = {
+    styl: ['assets/**/*.styl'],
+    css: ['assets/**/*.css'],
+    coffee: ['assets/**/*.coffee'],
+    js: ['assets/**/*.js'],
+    bower: ['bower.json', '.bowerrc']
+};
+
+src.styles = src.styl.concat(src.css);
+src.scripts = src.coffee.concat(src.js);
+
+var publishdir = 'public'
+var dist = {
+    all: [publishdir + '/**/*'],
+    css: publishdir + '/static/',
+    js: publishdir + '/static/',
+    vendor: publishdir + '/static/'
 };
 
 gulp.task('lint', function () {
@@ -20,7 +37,23 @@ gulp.task('lint', function () {
 
 // task
 gulp.task('bower-files', function () {
-    bowerFiles().pipe(gulp.dest("./Scripts/lib"));
+    var jsFilter = gulpFilter('**/*.js')
+    var cssFilter = gulpFilter('**/*.css')
+    return bower()
+      .pipe(jsFilter)
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest(dist.js))
+      .pipe(jsFilter.restore())
+      .pipe(cssFilter)
+      .pipe(concat('vendor.css'))
+      .pipe(gulp.dest(dist.css))
+      .pipe(cssFilter.restore())
+      .pipe(rename(function (path) {
+          if (~path.dirname.indexOf('fonts')) {
+              path.dirname = '/fonts'
+          }
+      }))
+      .pipe(gulp.dest(dist.vendor));
 });
 
 gulp.task('build', ['bower-files','lint'])
