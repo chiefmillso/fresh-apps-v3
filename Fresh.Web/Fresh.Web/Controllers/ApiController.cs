e134fc9144da42024e545d792d3e23626d2eddf4
David Miller <david@davidmiller.co>
2015-02-20 05:23:25 +1100

    incorporated autofac bootstrapping

--- Fresh.Web/Fresh.Web/Controllers/ApiController.cs
+++ Fresh.Web/Fresh.Web/Controllers/ApiController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Web.Http;
 using Fresh.Core.Web.Security.Attributes;
+using MediatR;
 
 namespace Fresh.Web.Controllers
 {
     [RequireHttpsInProduction]
     public class FreshController : ApiController
     {
-        [HttpGet]
+        private readonly IMediator _mediator;
+
+        public FreshController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [System.Web.Http.HttpGet]
         [Route("api/echo/{input}")]
         public string Echo(string input)
         {
--- Fresh.Web/Fresh.Web/Global.asax.cs
+++ Fresh.Web/Fresh.Web/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System.Web;
+﻿using System.Reflection;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Fresh.Core.Web.App_Start;
 using Fresh.Web.App_Start;
 
 namespace Fresh.Web
@@ -10,6 +12,10 @@ namespace Fresh.Web
     {
         protected void Application_Start()
         {
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Assemblies.Add(Assembly.GetExecutingAssembly());
+
+            AutofacConfig.Configure(bootstrapper);
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
